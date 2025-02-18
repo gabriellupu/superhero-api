2e282be61503a4b580c4b3a8a9cdc1fa64f6ff22
Gabriel Lupu <lupugabriel1@gmail.com>
2025-02-18 19:04:34 +0200

    Add Jest Test

--- src/superheroes/superheroes.controller.spec.ts
+++ src/superheroes/superheroes.controller.spec.ts
@@ -1,18 +1,40 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test, type TestingModule } from '@nestjs/testing';
 import { SuperheroesController } from './superheroes.controller';
+import { SuperheroesService } from './superheroes.service';
 
 describe('SuperheroesController', () => {
   let controller: SuperheroesController;
+  let service: SuperheroesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [SuperheroesController],
+      providers: [SuperheroesService],
     }).compile();
 
     controller = module.get<SuperheroesController>(SuperheroesController);
+    service = module.get<SuperheroesService>(SuperheroesService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('findAll', () => {
+    it('should return an array of superheroes sorted by humility score', () => {
+      const result = [
+        {
+          id: 1,
+          name: 'Super Humble',
+          superpower: 'Humility',
+          humilityScore: 10,
+        },
+        {
+          id: 2,
+          name: 'Captain Modesty',
+          superpower: 'Modesty',
+          humilityScore: 8,
+        },
+      ];
+      jest.spyOn(service, 'findAll').mockImplementation(() => result);
+
+      expect(controller.findAll()).toBe(result);
+    });
   });
 });
